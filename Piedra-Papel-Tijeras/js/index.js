9eb41a01e5010d7e85e95a18695bc01f420e54bd
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-03-06 12:55:18 -0500

    Se crea primera sección de la logica

--- Piedra-Papel-Tijeras/js/index.js
+++ Piedra-Papel-Tijeras/js/index.js
@@ -6,5 +6,11 @@ let computerScore = 0;
 //Creamos función de lógica de juego
 
 function playGame(userChoice){
-    
+    const choices = ['piedra', 'papel', 'tijera'];
+    const computerChoice = choices[Math.floor(Math.random()*3)];
+
+    let result = '';
+    if(){
+        
+    }
 }
