f065537a738a34483d562f0e3b4ea14e1d7ae844
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-03-06 15:36:55 -0500

    Se agrega boton de recargar

--- Piedra-Papel-Tijeras/js/index.js
+++ Piedra-Papel-Tijeras/js/index.js
@@ -6,11 +6,42 @@ let computerScore = 0;
 //Creamos función de lógica de juego
 
 function playGame(userChoice){
+    //elige las opciones de forma aleatoria
     const choices = ['piedra', 'papel', 'tijera'];
     const computerChoice = choices[Math.floor(Math.random()*3)];
 
     let result = '';
-    if(){
-        
+    if(userChoice == computerChoice){
+        result = 'Han sacado lo mismo, es un empate de '+userChoice;
     }
+    else if((userChoice == 'piedra' && computerChoice == 'tijera') ||
+            (userChoice == 'tijera' && computerChoice == 'papel') ||
+            (userChoice == 'papel' && computerChoice == 'piedra') ){
+                result = 'Haz ganado la ronda con '+userChoice+' contra '+computerChoice;
+                playerScore++;
+    }
+    else{
+        result = 'Haz perdido con '+userChoice+' contra '+computerChoice;
+        computerScore++;
+    }
+
+    //Enlazamos a nuestro HTML y ponemos el contenido de la variable result
+    document.getElementById('result').textContent = result;
+    document.getElementById('score').textContent = 'Jugador: '+playerScore+ ' | Computadora: '+computerScore;
+
+    //lógica para acabar el juego
+    if(playerScore == 3 || computerScore == 3){
+        endGame();
+    }
+}
+
+//función de acabar el juego
+function endGame(){
+    const finalResult = playerScore == 3 ? 'Felicidades, haz ganado' : 'lo siento, haz perdido';
+    document.getElementById('result').textContent = finalResult;
+
+    //Aca deshabilitamos los botones cuando est en  3
+    document.getElementById('btnPiedra').disabled = true;
+    document.getElementById('btnPapel').disabled = true;
+    document.getElementById('btnTijera').disabled = true;
 }
