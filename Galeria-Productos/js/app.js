7282e9fd010285becccc4c0529e19f7dcaa9399f
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-03-26 17:36:03 -0500

    Se inicia con la creación de clases

--- Galeria-Productos/js/app.js
+++ Galeria-Productos/js/app.js
@@ -1,1 +1,21 @@
-alert('prueba')
+class product{
+    constructor(name, price, year){
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+}
+
+class ui{
+    addproduct(){
+
+    }
+
+    deleteproduct(){
+
+    }
+
+    showmessage(){
+        
+    }
+}
