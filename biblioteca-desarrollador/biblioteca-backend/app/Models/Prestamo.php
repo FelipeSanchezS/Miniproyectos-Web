39a8ba923c4d4a7bec26fb720e7b28231fa0a507
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-04-24 13:17:11 -0500

    FInalizamos la unión de modelos

--- biblioteca-desarrollador/biblioteca-backend/app/Models/Prestamo.php
+++ biblioteca-desarrollador/biblioteca-backend/app/Models/Prestamo.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Prestamo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'libro_id', 'fecha_prestamo', 'fecha_devolucion'];
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function libro()
+    {
+        return $this->belongsTo(Libro::class);
+    }
 }
+
