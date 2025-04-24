d7c80c02451cf36d35f492159f95d0e4b4bca2f2
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-04-24 13:15:35 -0500

    Se relacionan los modelos libros y prestamos

--- biblioteca-desarrollador/biblioteca-backend/app/Models/Libro.php
+++ biblioteca-desarrollador/biblioteca-backend/app/Models/Libro.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Libro extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['titulo', 'autor', 'genero', 'disponible'];
+
+    public function prestamos()
+    {
+        return $this->hasMany(Prestamo::class);
+    }
 }
