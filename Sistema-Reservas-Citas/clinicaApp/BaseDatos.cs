44e6011e5ee9be21c3166a689093649ab11bdb28
Luis Felipe Sanchez Sanchez <155125471+FelipeSanchezS@users.noreply.github.com>
2025-05-12 12:36:20 -0500

    Agregamos sección de base de datos y el config

---
+++ Sistema-Reservas-Citas/clinicaApp/BaseDatos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ClinicaApp
+{
+    public class DB
+    {
+        private static string connectionString = "server=localhost;user=root;database=reservas;password=root;";
+        public static MySqlConnection GetConnection()
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error de conexión: " + ex.Message);
+            }
+            return conn;
+        }
+    }
+}
+
